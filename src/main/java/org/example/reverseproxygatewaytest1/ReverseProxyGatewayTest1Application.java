8dafcaeb9a8c826761b8e80c72f67a2350e8c604
Lutencu Octavian <octavv2001@yahoo.com>
2024-12-03 08:24:06 +0200

    first commit

---
+++ src/main/java/org/example/reverseproxygatewaytest1/ReverseProxyGatewayTest1Application.java
@@ -0,0 +1,13 @@
+package org.example.reverseproxygatewaytest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseProxyGatewayTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverseProxyGatewayTest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/reverseproxygatewaytest1/ReverseProxyGatewayTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.reverseproxygatewaytest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverseProxyGatewayTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
